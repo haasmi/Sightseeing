427282c7801d1800acbda81ac3c0e30a2d5cc1ac
haasmi <64859108+haasmi@users.noreply.github.com>
2020-05-14 13:04:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 
 int main() {
 	Place* Zuhause = new Place("Zuhause", 1, 1);
+	Place* Urlaub = new Place("Hotel", 27, 14);
 	Sightseeing* Eiffelturm = new Sightseeing("Eiffelturm", 12, 12, "bunte Lichter");
 	Toilet* Schule = new Toilet("Schulklo", 17, 3, "ekelig");
 	Schule->visit();
 	Eiffelturm->visit();
 	Zuhause->visit();
+	Urlaub->visit();
 }
